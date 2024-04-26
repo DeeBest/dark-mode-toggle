8f67dc428323c960acfcaae2803bad2d9e5164b5
DeeBest <simphiwedladla8@gmail.com>
2024-04-26 18:40:30 +0200

    created function that will be responsible for toggling dark mode

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const footerParagraph = document.getElementById('footer-paragraph');
 const currentDate = new Date().getFullYear();
 footerParagraph.textContent = `Created by \xA9Simphiwe ${currentDate}.`;
+
+const body = document.querySelector('body');
+const darkModeInput = document.getElementById('dark-mode-input');
+
+darkModeInput.checked = false;
+updateTheme();
+
+function updateTheme() {
+  if (darkModeInput.checked) {
+    body.style.backgroundColor = 'hsl(24, 10%, 10%)';
+    body.style.color = 'hsl(20, 6%, 90%)';
+  } else {
+    body.style.backgroundColor = 'hsl(20, 6%, 90%)';
+    body.style.color = 'hsl(24, 10%, 10%)';
+  }
+}
